a1a36b2889500f4a370a87025cd5dcf008f56921
PTRNEE <159671317+PTRNEE@users.noreply.github.com>
2026-03-25 15:15:37 +0700

    use env for socket url

--- client/src/socket.js
+++ client/src/socket.js
@@ -9,7 +9,7 @@ const playerId = (() => {
   return id;
 })();
 
-export const socket = io("http://localhost:3000", {
+export const socket = io(import.meta.env.VITE_API_URL, {
   transports: ["websocket"],
   auth: { playerId },
 });
